0f8f4ac6074ab84c5b563b185b49448c27bbd93c
Bonny <tonylf234@gmail.com>
2026-06-17 00:52:58 -0400

    fix: force bSimulatePhysics = true in BeginPlay for Chaos Mover

--- Source/ProjectF/Private/GSPawn.cpp
+++ Source/ProjectF/Private/GSPawn.cpp
@@ -34,6 +34,7 @@ void AGSPawn::BeginPlay()
 		CapsuleComponent->SetMobility(EComponentMobility::Movable);
 		CapsuleComponent->SetCollisionProfileName(TEXT("Pawn"));
 		CapsuleComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		CapsuleComponent->SetSimulatePhysics(true);
 	}
 
 	Super::BeginPlay();
